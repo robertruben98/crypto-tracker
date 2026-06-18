8cb36a3f4c27f2171701e06e6bf5b60fd7452f81
Robert <robert.benitez@treew.com>
2026-06-18 14:28:30 +0200

    test: remove unused waitFor import in useMarkets test

--- src/hooks/useMarkets.test.ts
+++ src/hooks/useMarkets.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest'
-import { renderHook, waitFor, act } from '@testing-library/react'
+import { renderHook, act } from '@testing-library/react'
 import { useMarkets } from './useMarkets'
 import * as api from '../api/coingecko'
 
